7149062dbfbb9762bb8475a4d1f200ca51cadef8
maksymtymchenko <makstimchenk@gmail.com>
2024-04-21 16:08:58 +0300

    add build command

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { config } from "./config.js";
 const app = express()
 
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 
 app.use('/', appRouter)
 app.use('/api', sentenceRouter)
